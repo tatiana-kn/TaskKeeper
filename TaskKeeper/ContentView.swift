4a20eac7f26f576cff59c5aab1df0a8a7678357a
tatiana-kn <123388284+tatiana-kn@users.noreply.github.com>
2024-03-22 08:31:15 +0400

    Initial Commit

---
+++ TaskKeeper/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TaskKeeper
+//
+//  Created by Tia M on 3/22/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TaskKeeper/TaskKeeperApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskKeeperApp.swift
+//  TaskKeeper
+//
+//  Created by Tia M on 3/22/24.
+//
+
+import SwiftUI
+
+@main
+struct TaskKeeperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
